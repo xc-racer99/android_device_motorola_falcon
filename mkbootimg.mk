ae54cc7171fcb44d1b7141b12f019c5bd0c7667d
xc-racer99 <xc-racer2@live.ca>
2015-09-03 21:14:38 -0700

    Update TWRP config and compress ramdisk
    Makes the recovery.img below the necessary size
    
    Thanks to h2o64 for the mkbootimg, see https://github.com/h2o64/android_device_motorola_falcon/blob/8d862d7b54c82ccde128dd93aee1f1c30a359721/mkbootimg.mk for original

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -43,11 +43,16 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTAL
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
+LZMA_RECO := $(PRODUCT_OUT)/ramdisk-recovery-lzma.img
+
+$(LZMA_RECO): $(recovery_ramdisk)
+	gunzip -f < $(recovery_ramdisk) | lzma -e > $@
+
 ## Overload recoveryimg generation: Same as the original, + --dt arg
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
-		$(recovery_ramdisk) \
+		$(LZMA_RECO) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@ --ramdisk $(LZMA_RECO)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
